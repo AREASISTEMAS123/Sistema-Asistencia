7706ba65a68ef04cc94143fedae2c1d2b63579ff
Caleb <reateguicevallosc@gmail.com>
2023-08-18 09:13:38 -0500

    feat: add database migrations

---
+++ backendv2/database/migrations/2023_08_18_124805_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('username');
+            $table->string('surname');
+            $table->string('email');
+            $table->string('password');
+            $table->boolean('status');
+            $table->string('dni');
+            $table->string('profile');
+            $table->string('cellphone');
+            $table->string('shift');
+            $table->date('birthday');
+            $table->string('image');
+            $table->date('date_start');
+            $table->date('date_end');
+
+            $table->unsignedBigInteger('cores_id');
+            $table->foreign('cores_id')->references('id')->on('cores')->onDelete('cascade');
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};

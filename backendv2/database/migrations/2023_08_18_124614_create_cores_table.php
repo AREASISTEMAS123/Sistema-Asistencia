eb73b336a57280e99d0ba08f51c944a3d36d6136
Caleb <reateguicevallosc@gmail.com>
2023-08-21 10:13:51 -0500

    fix: name error
    rodrigo mistake

--- backendv2/database/migrations/2023_08_18_124614_create_cores_table.php
+++ backendv2/database/migrations/2023_08_18_124614_create_cores_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
      */
     public function up()
     {
-
-
-
         Schema::create('cores', function (Blueprint $table) {
             $table->id();
             $table->string('name');

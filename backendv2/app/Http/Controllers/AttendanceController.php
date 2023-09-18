1918513dd14283084affb9cafb1e751fc53108ec
Rodrigo <torres.rodrigo.752@gmail.com>
2023-09-18 08:29:59 -0500

    Update AttendanceController.php
    retornar asistencia con fecha actual

--- backendv2/app/Http/Controllers/AttendanceController.php
+++ backendv2/app/Http/Controllers/AttendanceController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Services\AttendanceService;
 use App\Models\Attendance;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -46,9 +47,10 @@ class AttendanceController extends Controller
     {
         //Recogemos el ID del usuario logeado
         $user_id = auth()->id();
+        $date = Carbon::now();
 
         // Obtener el registro de asistencia del usuario para el usuario actualmente logeado
-        $attendance = Attendance::where('user_id', $user_id)->get();
+        $attendance = Attendance::where('user_id', $user_id)->where('date', $date)->first();
 
         //Retornamos la respuesta en formato JSON
         return response()->json(['attendance' => $attendance]);

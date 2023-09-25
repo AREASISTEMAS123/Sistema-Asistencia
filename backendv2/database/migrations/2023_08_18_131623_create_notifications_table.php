ac58a7d42d39098cd356244cf1a5fc7c03c5b25d
Rodrigo <torres.rodrigo.752@gmail.com>
2023-09-25 12:11:41 -0500

    Update 2023_08_18_131623_create_notifications_table.php

--- backendv2/database/migrations/2023_08_18_131623_create_notifications_table.php
+++ backendv2/database/migrations/2023_08_18_131623_create_notifications_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->string('mesage');
-            
+            $table->string('message');
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
